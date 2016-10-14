363b56bd3c0e8cd13413b23c7c5bf56e873b4e35
Daniel J. Hofmann <daniel@trvx.org>
2016-10-14 15:55:21 +0200

    Collapses Double OSRM <-> Engine <-> .. PImpl Indirection, Resolves #3019.

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -3,14 +3,6 @@
 #include "engine/engine_config.hpp"
 #include "engine/status.hpp"
 
-#include "engine/plugins/match.hpp"
-#include "engine/plugins/nearest.hpp"
-#include "engine/plugins/table.hpp"
-#include "engine/plugins/tile.hpp"
-#include "engine/plugins/trip.hpp"
-#include "engine/plugins/viaroute.hpp"
-
-#include "engine/datafacade/datafacade_base.hpp"
 #include "engine/datafacade/internal_datafacade.hpp"
 #include "engine/datafacade/shared_datafacade.hpp"
 
@@ -73,7 +65,14 @@ namespace engine
 
 Engine::Engine(const EngineConfig &config)
     : lock(config.use_shared_memory ? std::make_unique<storage::SharedBarriers>()
-                                    : std::unique_ptr<storage::SharedBarriers>())
+                                    : std::unique_ptr<storage::SharedBarriers>()),
+      route_plugin(config.max_locations_viaroute),       //
+      table_plugin(config.max_locations_distance_table), //
+      nearest_plugin(config.max_results_nearest),        //
+      trip_plugin(config.max_locations_trip),            //
+      match_plugin(config.max_locations_map_matching),   //
+      tile_plugin()                                      //
+
 {
     if (config.use_shared_memory)
     {
@@ -87,51 +86,36 @@ Engine::Engine(const EngineConfig &config)
         }
         query_data_facade = std::make_shared<datafacade::InternalDataFacade>(config.storage_config);
     }
-
-    // Register plugins
-    using namespace plugins;
-
-    route_plugin = std::make_unique<ViaRoutePlugin>(config.max_locations_viaroute);
-    table_plugin = std::make_unique<TablePlugin>(config.max_locations_distance_table);
-    nearest_plugin = std::make_unique<NearestPlugin>(config.max_results_nearest);
-    trip_plugin = std::make_unique<TripPlugin>(config.max_locations_trip);
-    match_plugin = std::make_unique<MatchPlugin>(config.max_locations_map_matching);
-    tile_plugin = std::make_unique<TilePlugin>();
 }
-
-// make sure we deallocate the unique ptr at a position where we know the size of the plugins
-Engine::~Engine() = default;
-Engine::Engine(Engine &&) noexcept = default;
-Engine &Engine::operator=(Engine &&) noexcept = default;
 
 Status Engine::Route(const api::RouteParameters &params, util::json::Object &result) const
 {
-    return RunQuery(lock, query_data_facade, params, *route_plugin, result);
+    return RunQuery(lock, query_data_facade, params, route_plugin, result);
 }
 
 Status Engine::Table(const api::TableParameters &params, util::json::Object &result) const
 {
-    return RunQuery(lock, query_data_facade, params, *table_plugin, result);
+    return RunQuery(lock, query_data_facade, params, table_plugin, result);
 }
 
 Status Engine::Nearest(const api::NearestParameters &params, util::json::Object &result) const
 {
-    return RunQuery(lock, query_data_facade, params, *nearest_plugin, result);
+    return RunQuery(lock, query_data_facade, params, nearest_plugin, result);
 }
 
 Status Engine::Trip(const api::TripParameters &params, util::json::Object &result) const
 {
-    return RunQuery(lock, query_data_facade, params, *trip_plugin, result);
+    return RunQuery(lock, query_data_facade, params, trip_plugin, result);
 }
 
 Status Engine::Match(const api::MatchParameters &params, util::json::Object &result) const
 {
-    return RunQuery(lock, query_data_facade, params, *match_plugin, result);
+    return RunQuery(lock, query_data_facade, params, match_plugin, result);
 }
 
 Status Engine::Tile(const api::TileParameters &params, std::string &result) const
 {
-    return RunQuery(lock, query_data_facade, params, *tile_plugin, result);
+    return RunQuery(lock, query_data_facade, params, tile_plugin, result);
 }
 
 } // engine ns
